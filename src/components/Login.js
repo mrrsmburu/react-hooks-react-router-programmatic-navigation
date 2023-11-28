02973a70bf7e52d767b5a54887355978a45ebc04
mrrsmburu <mrrsmburu@gmail.com>
2023-11-28 08:25:16 +0300

    completed assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,11 +17,22 @@ function Login({ setIsLoggedIn }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        onLogin(user);
 
     setIsLoggedIn(true);
 
     // after logging the user in, redirect to the home page!
     history.push("/");
+    });
   }
 
   return (
